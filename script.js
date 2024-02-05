2ee127ad1fd59e838ec24ed1227822ba3cde0ec0
MohdKukshiwala <153647187+MohammedKukshiwala@users.noreply.github.com>
2024-02-05 20:24:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction_yes() {
+    document.getElementById("demo").innerHTML = "I know you will say yes only, Love you too Thank you.";
+  }
+
+  function myFunction_no() {
+    document.getElementById("demo").innerHTML = "Aree yaar, whats is this. I am sad now";
+  }
